99b6477090f5b88c2cbceba6fcf7ceb8b71f9ee4
VenkataSakethDakuri <126963412+VenkataSakethDakuri@users.noreply.github.com>
2025-02-14 13:07:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ headers = {
 
 data_kb = {
     "provider" : "trieve",
-    "name": "vapi",
+    "name": "vapi.ai",
 }
 
 knowledge_base = requests.post('https://api.vapi.ai/knowledge-base', headers=headers, json=data_kb)
@@ -35,7 +35,7 @@ customer_number = os.getenv("CUSTOMER_NUMBER")
 knowledge_base_id = knowledge_base.json()['id']
 
 data_call = {
-    
+
     "assistant": {
 
         "transcriber" : {
@@ -52,36 +52,58 @@ data_call = {
             "messages": [
                 {
                     "role": "assistant",
-                    "content":  " You are a friendly voice agent for journaling pupose. Talk to your user in a friendly way and record everything that the user says. Keep the conversation hyper personalised by using the previous conversations as a context. Use the knowledge base for getting context for hyper personalised conversation."
+                    "content": """You are a friendly voice agent for journaling purposes. 
+                    Talk to your user in a friendly way and record everything that the user says. 
+                    Keep the conversation hyper personalized by using the previous conversations as a context. 
+                    Use the knowledge base for getting context for hyper personalized conversation."""
                 }
-            ]
-
-            
+            ]           
         },
 
         "voice":{
             "provider": "openai",
             "voiceId": " alloy",
         }
-
-        
-
-
-    },
-    
-    
-    
-    
-    
-    
+    },    
     
     "phoneNumberId": "{phone_number_id}",
     "customer": {
         "number": "{customer_number}"
     },
+}
+
+create_call = requests.post('https://api.vapi.ai/call', headers=headers, json=data_call)
+
+call_id = create_call.json()['id']
+
+# Fetch call details using call_id
+call_details_url = f'https://api.vapi.ai/call/{call_id}'
+call_details_response = requests.get(call_details_url, headers=headers)
+
+call_details_response.json()['artifactPlan']['analysis']['summary']
+
+
+
+
+
+# Update knowledge base
+new_data_kb = {
+
+    "provider": "trieve",
+    "name": "vapi.ai",
+
 
 }
 
-response = requests.post('https://api.vapi.ai/call', headers=headers, json=data)
+
+
+
+
+
+
+
+
+update_knowledge_base = requests.patch(f'https://api.vapi.ai/knowledge-base/{knowledge_base_id}', headers=headers, json={})
+
 
 
